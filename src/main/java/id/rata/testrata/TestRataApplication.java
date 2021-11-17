84b78212741951a8be6b567b8931e2bd6abd8868
pashaibrhm <ariefpasa.ibrahim.api@gmail.com>
2021-11-18 06:05:10 +0700

    First Commit

---
+++ src/main/java/id/rata/testrata/TestRataApplication.java
@@ -0,0 +1,13 @@
+package id.rata.testrata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/rata/testrata/TestRataApplicationTests.java
@@ -0,0 +1,13 @@
+package id.rata.testrata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
